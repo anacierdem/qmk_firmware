f66fb420ec0128c295fbe75fc8262e9368d3a768
Ali Naci Erdem <anacierdem@gmail.com>
2023-04-12 22:20:53 +0300

    My custom configuration

--- keyboards/crkbd/rev1/config.h
+++ keyboards/crkbd/rev1/config.h
@@ -18,9 +18,19 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 #pragma once
 
-#ifdef RGB_MATRIX_ENABLE
-#    define RGB_MATRIX_LED_COUNT 54
-#    define RGB_MATRIX_SPLIT \
-        { 27, 27 }
-#    define SPLIT_TRANSPORT_MIRROR
+/* ws2812 RGB LED */
+#define RGB_DI_PIN      D3
+
+#ifdef RGBLIGHT_ENABLE
+#    define RGBLED_NUM 42 // Number of LEDs
+#    define RGBLED_SPLIT \
+        { 21, 21 }
+#    define RGBLIGHT_SPLIT
 #endif
+
+// #ifdef RGB_MATRIX_ENABLE
+// #    define RGB_MATRIX_LED_COUNT 54
+// #    define RGB_MATRIX_SPLIT \
+//         { 27, 27 }
+// #    define SPLIT_TRANSPORT_MIRROR
+// #endif
